ff976539250b6dd307bc5f17d06bdce0cab764b7
Cezary Olborski <cezary.olborski@gmail.com>
2025-01-12 13:16:18 +0800

    DTA-1733 - feat: Data Model (#3)
    * feat: Docker + db
    
    * feat: Config fix & SeaORM init
    
    * feat: Data Model + migration
    
    * feat: Data model + pair_swap concept

---
+++ src/config.rs
@@ -0,0 +1,130 @@
+use config::{Config, File};
+use once_cell::sync::Lazy;
+use serde::Deserialize;
+use std::env;
+use std::error::Error;
+use std::fmt;
+use std::sync::Arc;
+
+#[derive(Debug, Deserialize)]
+pub struct DefaultConfig {
+    pub server_port_http: u16,
+    pub db_url: String,
+    pub db_username: String,
+    pub db_password: String,
+    pub db_min_connections: u32,
+    pub db_max_connections: u32,
+    pub db_sql_logging: bool,
+    pub data_refresh_interval: u64,
+    pub rpc_url: String,
+    pub tx_log_start_block_number: u64,
+    pub calculation_window: u16,
+    pub cdi_fuel_token_gateway: String,
+    pub cdi_mira_amm: String,
+    pub mira_swap_event: String,
+    pub mira_create_pool: String,
+    pub mira_total_supply: String,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct AppConfig {
+    pub default: DefaultConfig,
+}
+
+pub enum EnvVar {
+    ServerPortHTTP,
+    DbUrl,
+    DBUsername,
+    DBPassword,
+    DBMinConnections,
+    DBMaxConnections,
+    DBSQLLogging,
+    DataRefreshInterval,
+    RpcUrl,
+    TxLogStartBlockNumber,
+    CalculationWindow,
+    CdiFuelTokenGateway,
+    CdiMiraAmm,
+    MiraSwapEvent,
+    MiraCreatePool,
+    MiraTotalSupply,
+}
+
+// Implement Display for EnvVar to convert to string
+impl fmt::Display for EnvVar {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let name = match self {
+            EnvVar::ServerPortHTTP => "SERVER_PORT_HTTP",
+            EnvVar::DbUrl => "DB_URL",
+            EnvVar::DBUsername => "DB_USERNAME",
+            EnvVar::DBPassword => "DB_PASSWORD",
+            EnvVar::DBMinConnections => "DB_MIN_CONNECTIONS",
+            EnvVar::DBMaxConnections => "DB_MAX_CONNECTIONS",
+            EnvVar::DBSQLLogging => "DB_SQL_LOGGING",
+            EnvVar::DataRefreshInterval => "DATA_REFRESH_INTERVAL",
+            EnvVar::RpcUrl => "RPC_URL",
+            EnvVar::TxLogStartBlockNumber => "TX_LOG_START_BLOCK_NUMBER",
+            EnvVar::CalculationWindow => "CALCULATION_WINDOW",
+            EnvVar::CdiFuelTokenGateway => "CDI_FUEL_TOKEN_GATEWAY",
+            EnvVar::CdiMiraAmm => "CDI_MIRA_AMM",
+            EnvVar::MiraSwapEvent => "MIRA_SWAP_EVENT",
+            EnvVar::MiraCreatePool => "MIRA_CREATE_POOL",
+            EnvVar::MiraTotalSupply => "MIRA_TOTAL_SUPPLY",
+        };
+        write!(f, "{}", name)
+    }
+}
+
+impl EnvVar {
+    pub fn get_value<T: std::str::FromStr>(&self, default: T) -> T
+    where
+        T::Err: std::fmt::Debug,
+    {
+        env::var(self.to_string())
+            .ok()
+            .and_then(|val| val.parse().ok())
+            .unwrap_or(default)
+    }
+}
+
+// Lazy static configuration loading
+pub static CONFIG: Lazy<Arc<AppConfig>> = Lazy::new(|| {
+    log::info!("Loading configuration...");
+    match load_config_from_env_or_file() {
+        Ok(config) => Arc::new(config),
+        Err(e) => {
+            log::error!("Failed to load configuration: {:?}", e);
+            panic!("Failed to load configuration: {:?}", e);
+        }
+    }
+});
+
+pub fn load_config_from_env_or_file() -> Result<AppConfig, Box<dyn Error>> {
+    let mut settings = Config::builder()
+        .add_source(File::with_name("resources/config.toml").required(false))
+        .build()?;
+
+    let mut config: AppConfig = settings.try_deserialize()?;
+
+    // Override with environment variables
+    config.default.server_port_http =
+        EnvVar::ServerPortHTTP.get_value(config.default.server_port_http);
+    config.default.db_url = EnvVar::DbUrl.get_value(config.default.db_url.clone());
+    config.default.db_username = EnvVar::DBUsername.get_value(config.default.db_username.clone());
+    config.default.db_password = EnvVar::DBPassword.get_value(config.default.db_password.clone());
+    config.default.db_min_connections =
+        EnvVar::DBMinConnections.get_value(config.default.db_min_connections);
+    config.default.db_max_connections =
+        EnvVar::DBMaxConnections.get_value(config.default.db_max_connections);
+    config.default.db_sql_logging =
+        EnvVar::DBSQLLogging.get_value(config.default.db_sql_logging);
+    config.default.data_refresh_interval =
+        EnvVar::DataRefreshInterval.get_value(config.default.data_refresh_interval);
+    config.default.rpc_url = EnvVar::RpcUrl.get_value(config.default.rpc_url.clone());
+    config.default.tx_log_start_block_number =
+        EnvVar::TxLogStartBlockNumber.get_value(config.default.tx_log_start_block_number);
+    config.default.calculation_window =
+        EnvVar::CalculationWindow.get_value(config.default.calculation_window);
+
+    Ok(config)
+}
--- src/ports/tx_monitor.rs
+++ src/ports/tx_monitor.rs
@@ -1,3 +1,4 @@
+use std::net::ToSocketAddrs;
 use std::str::FromStr;
 use fuels::prelude::*;
 use std::time::Duration;
@@ -6,6 +7,7 @@ use fuels::tx::TxParameters;
 use fuels::types::BlockHeight;
 use fuel_tx::Input;
 use fuels::core::codec::{ABIDecoder, DecoderConfig};
+use fuels::types::coin_type_id::CoinTypeId::UtxoId;
 use fuels::types::output::Output;
 use fuels::types::param_types::ParamType;
 use num_traits::AsPrimitive;
@@ -108,11 +110,11 @@ impl TxMonitor{
                                     //let asset_id = mint_tx.mint_asset_id();
                                 },
                                 TransactionType::Script(script_tx) => {
-                                    log::info!("SCRIPT");
+                                    //log::info!("SCRIPT");
                                     let mira_contract_id = ContractId::from_str("0x2e40f2b244b98ed6b8204b3de0156c6961f98525c8162f80162fcf53eebd90e7")?;
                                     for input in script_tx.inputs() {
                                         let cid = input.contract_id();
-                                        log::info!("CID: {:?}",cid);
+                                        //log::info!("CID: {:?}",cid);
                                         if cid.is_some(){
                                             if mira_contract_id == cid.unwrap().clone() {
 
@@ -140,13 +142,22 @@ impl TxMonitor{
                                                             match MiraEvent::from_u64(log_id) {
                                                                 Some(MiraEvent::Swap) => {
                                                                     log::info!("SwapEvent");
-                                                                    log::info!("{:?}",receipt);
+                                                                    log::info!("BlockID: {}", block_height );
+                                                                    log::info!("TX: {}",tx);
+                                                                    log::info!("Input: {}",input.utxo_id().unwrap_or(&Default::default()));
+                                                                    //log::info!("{:?}",receipt);
                                                                     let event = SwapEvent::try_from(receipt.data().unwrap()).unwrap();
-                                                                    log::info!("{:?}",event);
-                                                                    let asset_0_id = get_token_details_by_asset_id_2(&provider, &event.pool_id.0).await?;
-                                                                    log::info!("A0: {:?}",asset_0_id);
-                                                                    let asset_1_id = get_token_details_by_asset_id_2(&provider, &event.pool_id.1).await?;
-                                                                    log::info!("A0: {:?}",asset_1_id);
+                                                                    //log::info!("{:?}",event);
+                                                                    if let Some(asset_0_id) = get_token_details_by_asset_id(&provider, &event.pool_id.0).await? {
+                                                                        if let Some(asset_1_id) = get_token_details_by_asset_id(&provider, &event.pool_id.1).await?{
+                                                                            log::info!("A0: {:?}",asset_0_id);
+                                                                            log::info!("A0 amount: IN:{}, OUT:{}", &event.asset_0_in, &event.asset_0_out);
+                                                                            log::info!("A1: {:?}",asset_1_id);
+                                                                            log::info!("A1 amount: IN:{}, OUT:{}", &event.asset_1_in, &event.asset_1_out);
+                                                                        }
+                                                                    }else{
+                                                                        continue;
+                                                                    }
                                                                 }
                                                                 Some(MiraEvent::CreatePool) => {
                                                                     log::info!("CreatePoolEvent");
@@ -249,67 +260,53 @@ fn extract_asset_id(output: &Output) -> Option<&AssetId> {
 
 //Token methods
 
-async fn get_token_details_by_asset_id_2(provider: &Provider, asset_id: &AssetId) -> Result<TokenDetails>{
-
-    let mut wallet = WalletUnlocked::new_random(None);
-    wallet.set_provider(provider.clone());
-
-    let p_asset_id_0: AssetId = AssetId::from_str("0x1d5d97005e41cae2187a895fd8eab0506111e0e2f3331cd3912c15c24e3c1d82").unwrap_or(AssetId::zeroed());
-    let p_asset_id_1: AssetId = AssetId::from_str("0x286c479da40dc953bddc3bb4c453b608bba2e0ac483b077bd475174115395e6b").unwrap_or(AssetId::zeroed());
-
-    let mira_cid = ContractId::from_str(MIRA_AMM_2_CID).unwrap_or(ContractId::zeroed());
-
-    let mira_contract = MiraV1Core::new(mira_cid,wallet);
-
-    let token_symbol
-        = mira_contract.methods().total_assets()
-        .simulate(Execution::StateReadOnly).await?;
-
-    log::info!("TS: {:?}",token_symbol);
-
-    let token_details = TokenDetails{
-        name: "".to_string(),
-        symbol: "".to_string(),
-        decimals: 0,
-    };
-
-    Ok(token_details)
-
-}
-
-async fn get_token_details_by_asset_id(provider: &Provider,asset_id: &AssetId) -> Result<TokenDetails>{
+async fn get_token_details_by_asset_id(provider: &Provider,asset_id: &AssetId) -> Result<Option<TokenDetails>>{
     let mut wallet = WalletUnlocked::new_random(None);
     wallet.set_provider(provider.clone());
 
     let contract_id = ContractId::from_str(FUEL_TOKEN_GATEWAY_CID).unwrap_or(ContractId::zeroed());
     let fuel_token_gateway = FuelTokenGateway::new(contract_id,wallet);
 
-    //let asset_id_h: AssetId = AssetId::from_str(asset_id).unwrap_or(AssetId::zeroed());
-
     //TODO: There has to be more efficient way to take all this data at once
 
-    let token_name = fuel_token_gateway.methods().name(asset_id.clone()).with_contract_ids(&[
+
+
+    let response = fuel_token_gateway.methods().name(asset_id.clone()).with_contract_ids(&[
         Bech32ContractId::from(ContractId::from_str("0x0ceafc5ef55c66912e855917782a3804dc489fb9e27edfd3621ea47d2a281156").unwrap_or(ContractId::zeroed())),
-    ]).simulate(Execution::StateReadOnly).await?.value.unwrap();
-    let token_symbol = fuel_token_gateway.methods().symbol(asset_id.clone()).with_contract_ids(&[
-        Bech32ContractId::from(ContractId::from_str("0x0ceafc5ef55c66912e855917782a3804dc489fb9e27edfd3621ea47d2a281156").unwrap_or(ContractId::zeroed())),
-    ]).simulate(Execution::StateReadOnly).await?.value.unwrap();
-    let token_decimals = fuel_token_gateway.methods().decimals(asset_id.clone()).with_contract_ids(&[
-        Bech32ContractId::from(ContractId::from_str("0x0ceafc5ef55c66912e855917782a3804dc489fb9e27edfd3621ea47d2a281156").unwrap_or(ContractId::zeroed())),
-    ]).simulate(Execution::StateReadOnly).await?.value.unwrap();
+    ]).simulate(Execution::StateReadOnly).await;
 
+    match response{
+        Ok(call_response) => {
+            match call_response.value {
+                Some(token_name) => {
+                    let token_symbol = fuel_token_gateway.methods().symbol(asset_id.clone()).with_contract_ids(&[
+                        Bech32ContractId::from(ContractId::from_str("0x0ceafc5ef55c66912e855917782a3804dc489fb9e27edfd3621ea47d2a281156").unwrap_or(ContractId::zeroed())),
+                    ]).simulate(Execution::StateReadOnly).await?.value.unwrap();
 
-    //log::info!("ASSET TN: {:?}",token_name);
+                    let token_decimals = fuel_token_gateway.methods().decimals(asset_id.clone()).with_contract_ids(&[
+                        Bech32ContractId::from(ContractId::from_str("0x0ceafc5ef55c66912e855917782a3804dc489fb9e27edfd3621ea47d2a281156").unwrap_or(ContractId::zeroed())),
+                    ]).simulate(Execution::StateReadOnly).await?.value.unwrap();
 
-    let token_details = TokenDetails{
-        name: token_name,
-        symbol: token_symbol,
-        decimals: token_decimals,
-    };
-
-    Ok(token_details)
+                    let token_details = TokenDetails{
+                        address: asset_id.to_string(),
+                        name: token_name,
+                        symbol: token_symbol,
+                        decimals: token_decimals,
+                    };
+                    Ok(Some(token_details))
+                },
+                None => {
+                    log::info!("No asset found - int");
+                    Ok(None)
+                },
+            }
+        }
+        Err(e) => {
+            log::info!("No asset found - ext");
+            Ok(None)
+        }
+    }
 }
-
 
 //Block methods
 
@@ -351,6 +348,7 @@ async fn get_mira_pool_metadata(provider: &Provider, pool_id: MiraPoolId) ->Resu
 
 #[derive(Debug, Deserialize)]
 struct TokenDetails {
+    address: String,
     name: String,
     symbol: String,
     decimals: u8,
